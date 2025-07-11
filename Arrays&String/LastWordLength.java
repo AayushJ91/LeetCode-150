6ed5467d34b92d23319a7e78e9914dc9d8cb85a9
Aayush <aayushjha2431@gmail.com>
2025-07-11 09:35:51 +0530

    D5

---
+++ Arrays&String/LastWordLength.java
@@ -0,0 +1,50 @@
+//Problem: return the length of the last word;
+//link: https://leetcode.com/problems/length-of-last-word/description/?envType=study-plan-v2&envId=top-interview-150
+//Source: LeetCode(58)
+//Topics: String
+//Diff: Easy
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LastWordLength {
+    public static int lengthOfLastWord(String s) {
+        s = s.trim();
+        if (!s.contains(" ")) {
+            return s.length();
+        }
+        List<String> list = new ArrayList<>();
+        StringBuilder str = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            System.out.println("i:"+i);
+            if (s.charAt(i) == ' ') {
+                list.add(str.toString());
+                str.setLength(0);
+            }
+            str.append(s.charAt(i));
+            System.out.println(list);
+            System.out.println(str);
+        }
+        list.add(str.toString());
+        System.out.println("list ou loop:"+list);
+        return list.getLast().length();
+    }
+
+    public static int length(String s) {
+        s = s.trim();
+        int len = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == ' '){
+                break;
+            }
+            len++;
+        }
+        return len;
+    }
+
+    public static void main(String[] args) {
+        String s = "abchd";
+        System.out.println(length(s));
+    }
+}
---
+++ Arrays&String/MaxProfit.java
@@ -0,0 +1,23 @@
+//Problem: given an array of prices of stock on ith day return the max profit we can make from that array;
+//link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/submissions/1693792071/?envType=study-plan-v2&envId=top-interview-150
+//Source: LeetCode(121)
+//Topics: Array
+//Diff: Easy
+
+public class MaxProfit {
+    //Exceeded the Time limit
+    public static int maxProfit(int[] prices) {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = i; j < prices.length; j++) {
+                int diff = prices[j] - prices[i];
+                max = Math.max(max, diff);
+            }
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        int[] prices = {7,6,4,3,1};
+        System.out.println(maxProfit(prices));
+    }
+}
